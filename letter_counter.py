6101090abfd5f3a2c25b7fee036ea6221cb5582e
Miguel Natal <61808499+migueljnatal@users.noreply.github.com>
2021-05-01 23:59:57 -0300

    Add files via upload

---
+++ letter_counter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 23:58:50 2021
+
+@author: MIGUEL
+"""
+
+text = input()
+dict = {}
+
+
+for char in dict:
+
+  if char not in dict:
+
+    dict[char] = 1
+
+  else:
+
+    dict[char] = dict[char] + 1
+
+print(dict)
